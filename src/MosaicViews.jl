04bcbc2d23a913a766ba528814daf51cde15961e
Maarten Pronk <evetion@users.noreply.github.com>
2020-04-10 08:20:13 +0000

    Compat for require_one_based_indexing on 1.1 (#16)
    As require_one_based_indexing is only introduced in Base in 1.2

--- src/MosaicViews.jl
+++ src/MosaicViews.jl
@@ -386,7 +386,7 @@ function _padded_cat(imgs; center, fillvalue, dims)
 end
 
 ### compat
-if VERSION < v"1.1"
+if VERSION <= v"1.1"
     require_one_based_indexing(A...) = !Base.has_offset_axes(A...) || throw(ArgumentError("offset arrays are not supported but got an array with index other than 1"))
 else
     require_one_based_indexing = Base.require_one_based_indexing
