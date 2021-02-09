d0e732ceae428578a8b9605419c70a91fbce1fc7
Tim Holy <tim.holy@gmail.com>
2021-02-09 04:58:05 -0600

    Distinguish `mosaic` and `mosaicview` in the documentation.

--- src/MosaicViews.jl
+++ src/MosaicViews.jl
@@ -336,7 +336,9 @@ end
     mosaic(A1, A2...; center=true, kwargs...)
     mosaic([A1, A2, ...]; center=true, kwargs...)
 
-Create a mosaic out of input arrays `A1`, `A2`, ....
+Create a mosaic out of input arrays `A1`, `A2`, .... `mosaic` is essentially
+a more flexible version of `cat` or `hvcat`; like them it makes a copy of
+the inputs rather than returning a "view."
 
 If `center` is set to `true`, then the padded arrays will be shifted
 to the center; if set to false, they shift to the top-left corner. This
